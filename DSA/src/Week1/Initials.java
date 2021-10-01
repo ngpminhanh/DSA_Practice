e4a1b1a5772331e2e74befc7ff1fb8d65da292e6
ngpminhanh <you@example.com>
2021-10-01 22:55:12 +0700

    changed

---
+++ DSA/src/Week1/Initials.java
@@ -0,0 +1,16 @@
+package Week1;
+
+public class Initials {
+    public static void main(String[] hahaha)
+    {
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           ** ");
+        System.out.println("**    ***        **         **     **         ** **         **  ");
+        System.out.println("**  ***          **          **     **       **   **       **   ");
+        System.out.println("*****            **          **      **     **     **     **    ");
+        System.out.println("**  ***          **          **       **   **       **   **     ");
+        System.out.println("**    ***        **         **         ** **         ** **      ");
+        System.out.println("**      ***      **        **           ***           ***       ");
+        System.out.println("**        ***    **********              *             *        ");
+    }
+}
