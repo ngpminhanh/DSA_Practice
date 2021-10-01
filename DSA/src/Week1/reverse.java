dd295d6a76a6ad46fe494fc9c109a209b694086b
ngpminhanh <you@example.com>
2021-10-01 22:44:22 +0700

    updated

--- DSA/src/Week1/reverse.java
+++ DSA/src/Week1/reverse.java
@@ -3,7 +3,7 @@ package week1;
 public class reverse {
     public static void main(String[] hihi)
     {
-        String name1 = "Alice";
+        String name1 = "Alie";
         String name2 = "Bob";
         String name3 = "Carol";
         System.out.println("Hi " + name3 + ", " + name2 + ", " + name1);
